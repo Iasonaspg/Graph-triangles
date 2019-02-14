a0b41fdc28c831f5cd5d186e33282db43d5bb693
iasonas <iasonaspav@gmail.com>
2019-02-14 11:34:50 +0200

    CSV Read minor changes

--- readCSV.c
+++ readCSV.c
@@ -4,23 +4,67 @@
 #include <ctype.h>
 #include <inttypes.h>
 #include <errno.h>
-#define leng 12
+#include <math.h>
+
 
 int split(char* str, char* delim, long* args);
 char *trim_space(char *in);
+void readCSV(char* fName, long* arr, long* tmp);
+int findLines(char* fName);
 
-int main(void){
+int main(int argc, char** argv){
+
+    char* fName = argv[1];
+
+    int leng = 3*findLines(fName); 
+    printf("Lines in the file are: %d\n",leng);   
+    
+    long* arr = malloc(leng*sizeof(long));
+    long* tmp = malloc(3*sizeof(long));
+    
+    readCSV(fName,arr,tmp);
+    
+    for (int i=0;i<9;i++){
+        // printf("Arr: %ld\n",arr[i]);
+    }
+
+    return 0; 
+}
+
+
+int findLines(char* fName){
 
     FILE * fp;
     char * line = NULL;
     size_t len = 0;
     ssize_t read;
 
-    long* arr = malloc(leng*sizeof(long));
-    long* tmp = malloc(3*sizeof(long));
-    fp = fopen("test.txt", "r");
-    if (fp == NULL)
+    fp = fopen(fName, "r");
+    if (fp == NULL){
+        printf("Could not open file\n");
         exit(EXIT_FAILURE);
+    }
+    
+    int i = 0;
+    while ((read = getline(&line, &len, fp)) != -1) {
+        i++;
+    }
+    fclose(fp);
+    return i;
+}
+
+void readCSV(char* fName, long* arr, long* tmp){
+
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen(fName, "r");
+    if (fp == NULL){
+        printf("Could not open file\n");
+        exit(EXIT_FAILURE);
+    }
 
     int i = 0;
     while ((read = getline(&line, &len, fp)) != -1) {
@@ -33,16 +77,8 @@ int main(void){
         }
         i++;
     }
-
-    for (int i=0;i<leng;i++){
-        printf("Arr: %ld\n",arr[i]);
-    }
-
     fclose(fp);
-
 }
-
-
 
 int split(char* str, char* delim, long* tmp){
     int i = 0;
@@ -52,9 +88,8 @@ int split(char* str, char* delim, long* tmp){
     while (token != NULL) {
         strNum = trim_space(token);
         // printf("Number: %ld\n", strtoimax(strNum,&endPtr,10));
-        tmp[i] = strtoimax(strNum,&endPtr,10);
-        // printf("Number: %ld\n", tmp[i]);
-        i++;
+        tmp[i++] = strtoimax(strNum,&endPtr,10);
+        // printf("Number: %ld\n", tmp[i-1]);
         token = strtok(NULL, delim);   
     }
     return i;
